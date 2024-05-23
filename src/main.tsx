39894c0d07cef84e5de942a1fcc28aa77b748cac
web3devdex <34705561+web3devdex@users.noreply.github.com>
2024-05-23 15:26:00 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+        <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/web3devdex/tma-vite-template/master/tonconnect-manifest.json">
     <App />
+              </TonConnectUIProvider>
   </React.StrictMode>,
 )
